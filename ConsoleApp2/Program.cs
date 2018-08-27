26968cbadf8d258c9e785b03bd9de43d705f26db
SARAR-R <Saru.cpt@gmail.com>
2018-08-26 17:40:27 -0700

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main()
         {
             Console.WriteLine("Hello World!");
-
+            Console.WriteLine("SecondLine");
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
