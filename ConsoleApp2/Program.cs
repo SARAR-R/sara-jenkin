3311d2f96a17e6870b3bedf9ceb277d8669ece81
Saraswathi Rajkumar <srajkumar@clouddev.epiqcorp.com>
2018-08-28 17:09:56 -0700

    Add Test

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main()
         {
             Console.WriteLine("Hello World!");
+            //Console.WriteLine("Second Line");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
---
+++ ConsoleTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp2;
+
+namespace ConsoleApp2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World!", "Hello World!");
+        }
+    }
+}
